1e74ed0d124207769ae15835dc8fbed16ce0d90c
Vassili Baranov <vassili.baranov@gmail.com>
2019-01-09 19:26:19 +0200

    change success_url

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from users.forms import UserRegistrationForm
 from users.models import User
@@ -7,5 +8,5 @@ from users.models import User
 class UserRegistrationView(generic.CreateView):
     model = User
     form_class = UserRegistrationForm
-    success_url = '/'
+    success_url = reverse_lazy('users:login')
     template_name = 'users/registration.html'
