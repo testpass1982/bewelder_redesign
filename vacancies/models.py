ea9f2c50f08d41045ce289bed99bbc8623edfe17
Anatoly Popov <popov.anatoly@gmail.com>
2019-01-10 19:51:38 +0300

    added levels many-to-many fieldto vacancy model

--- vacancies/models.py
+++ vacancies/models.py
@@ -4,15 +4,27 @@ from mainapp.models import Category
 from django.utils import timezone
 # Create your models here.
 
+class Level(models.Model):
+    LEVEL_CHOICES = (
+        ('I', '1 уровень'),
+        ('II', '2 уровень'),
+        ('III', '3 уровень'),
+        ('IV', '4 уровень'),
+    )
+    name = models.CharField(max_length=20, choices=LEVEL_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Vacancy(models.Model):
     """base class for vacancy model"""
     title = models.CharField(u"Название", max_length=80)
+    salary_min = models.IntegerField(u'Зарплата от', blank=True)
+    salary_max = models.IntegerField(u'Зарплата до', blank=True)
+    naks_att_level = models.ManyToManyField(Level) 
     short_description = models.CharField(
         u'Краткое описание вакансии', max_length=200, blank=True)
-    """пока оставлю описание в виде поля с поддержкой форматирования"""
     description =RichTextUploadingField(verbose_name='Описание вакансии') 
-    """я сам не знаю зачем нам нужна категория, добавляю ее чисто интуитивно
-    в будущем ее можно будет убрать или заменить на какую-то другую связь с другими моделями"""
     category = models.ForeignKey(
         Category, verbose_name='Категория', on_delete=models.CASCADE, blank=True)
     created_date = models.DateTimeField(u'Дата создания', default=timezone.now)
