d488a7df77f7d0e8d6f83e8a75a2f640ccc2dc00
thunderamur <thunder-amur@yandex.ru>
2019-01-14 22:40:44 +0900

    Class Based views

--- orgs/views.py
+++ orgs/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
-from .models import Employer
+from orgs.models import Employer
+from django.views import generic
 
 
-def list(request):
-    title = 'Список работодателей',
+EMPLOYERS_PER_PAGE = 5
 
-    employer_list = Employer.objects.all().order_by('short_name')
 
-    content = {
-        'title': title,
-        'employer_list': employer_list
-    }
-
-    return render(request, 'orgs/list.html', content)
+class EmployerListView(generic.ListView):
+    model = Employer
+    template_name = 'orgs/list.html'
+    context_object_name = 'employer_list'
+    paginate_by = EMPLOYERS_PER_PAGE
