02bf70229824e1edff9a47e2516beb1f0371b89b
Vassili Baranov <vassili.baranov@gmail.com>
2019-01-18 12:23:03 +0200

    create user update view

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registration/', views.UserRegistrationView.as_view(), name='registration'),
+    path('profile_update/', views.UserUpdateView.as_view(), name='profile_update'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.views import generic
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views import generic
 
 from users.forms import UserRegistrationForm
 from users.models import User
@@ -10,3 +12,14 @@ class UserRegistrationView(generic.CreateView):
     form_class = UserRegistrationForm
     success_url = reverse_lazy('users:login')
     template_name = 'users/registration.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(generic.UpdateView):
+    model = User
+    template_name = 'users/profile_update.html'
+    fields = 'first_name', 'last_name', 'email', 'date_of_birth'
+    success_url = reverse_lazy('main')
+    
+    def get_object(self):
+        return self.request.user
