461517c6682d4538b0d8c5bfd5be3d079fa645c4
thunderamur <thunder-amur@yandex.ru>
2019-01-19 11:11:15 +0900

    employer_data as Anatoly made

--- orgs/tests.py
+++ orgs/tests.py
@@ -9,12 +9,25 @@ class EmployerModelTest(TestCase):
         test_region = Region.objects.create(name='Region 1')
         city_1 = City.objects.create(name='City 1', region=test_region)
         city_2 = City.objects.create(name='City 2', region=test_region)
-        Employer.objects.create(name='Employer 1', short_name='Emp. 1', inn='123456789012', city=city_1,
-                                phone='123123', email='test@email.local')
-        Employer.objects.create(name='Employer 2', short_name='Emp. 2', inn='123456789012', city=city_1,
-                                phone='123123', email='test@email.local')
-        Employer.objects.create(name='Employer 1', short_name='Emp. 1', inn='123456789012', city=city_2,
-                                phone='123123', email='test@email.local')
+
+        employer_data = {
+            'name': 'Employer1',
+            'short_name': 'Emp. 1',
+            'inn': '123456789012',
+            'city': city_1,
+            'phone': '123',
+            'email': 'test@email.local',
+        }
+        Employer.objects.create(**employer_data)
+
+        employer_data_2 = employer_data
+        employer_data_2['name'] = 'Employer 2'
+        employer_data_2['short_name'] = 'Emp. 2'
+        Employer.objects.create(**employer_data_2)
+
+        employer_data_3 = employer_data
+        employer_data_3['city'] = city_2
+        Employer.objects.create(**employer_data_3)
 
     def test_employers_creation(self):
         employer_1_city_1 = Employer.objects.get(name='Employer 1', city__name='City 1')
