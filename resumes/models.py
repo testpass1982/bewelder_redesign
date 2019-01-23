1200a492ec9b9cfa129883cbd71ea892ec167ea2
Vassili Baranov <vassili.baranov@gmail.com>
2019-01-23 17:53:38 +0200

    add salary and experience_str properties to Resume

--- resumes/models.py
+++ resumes/models.py
@@ -17,3 +17,26 @@ class Resume(models.Model):
 
     def __str__(self):
         return 'Резюме: {} {}'.format(self.position.lower(), self.user.get_full_name())
+    
+    @property
+    def salary(self):
+        if self.salary_min and self.salary_max:
+            return '{}-{}'.format(self.salary_min, self.salary_max)
+        elif self.salary_min:
+            return 'от {}'.format(self.salary_min)
+        elif self.salary_max:
+            return 'до {}'.format(self.salary_max)
+        else:
+            return ''
+
+    @property
+    def experience_str(self):
+        if not self.experience:
+            return '0 лет'
+        mod = self.experience % 10
+        if mod == 1 and self.experience != 11:
+            return '{} год'.format(self.experience)
+        elif 1 < mod < 5 and (self.experience < 10 or self.experience > 20):
+            return '{} года'.format(self.experience)
+        
+        return '{} лет'.format(self.experience)
--- resumes/tests/test_models.py
+++ resumes/tests/test_models.py
@@ -1,16 +1,51 @@
-from django.test import TestCase
+import itertools
+
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 
 from resumes.models import Resume
-
 
 User = get_user_model()
 
 
 class ResumeTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user('foo@bar.com', first_name='Иван', last_name='Иванов')
+
     def test_str_resume(self):
-        user = User.objects.create_user('foo@bar.com', first_name='Иван', last_name='Иванов')
+        user = User.objects.first()
         resume = Resume.objects.create(user=user, position='Сварщик')
         expected = 'Резюме: сварщик Иван Иванов'
         result = str(resume)
         self.assertEqual(expected, result)
+
+    def test_salary_property(self):
+        user = User.objects.first()
+        resume = Resume.objects.create(user=user, position='Сварщик')
+        self.assertEqual(resume.salary, '')
+        resume.salary_min = 50000
+        self.assertEqual(resume.salary, 'от 50000')
+        resume.salary_max = 100000
+        self.assertEqual(resume.salary, '50000-100000')
+        resume.salary_min = 0
+        self.assertEqual(resume.salary, 'до 100000')
+
+    def test_experience_str(self):
+        user = User.objects.first()
+        resume = Resume.objects.create(user=user, position='Сварщик')
+        self.assertEqual(resume.experience_str, '0 лет')
+        for i, exp in ((1, '1 год'), (21, '21 год'), (31, '31 год')):
+            resume.experience = i
+            with self.subTest(i=i):
+                self.assertEqual(resume.experience_str, exp)
+        
+        for i in itertools.chain(range(5, 21), range(25, 31), range(35, 41)):
+            resume.experience = i
+            with self.subTest(i=i):
+                self.assertEqual(resume.experience_str, '{} лет'.format(i))
+
+        for i in itertools.chain(range(2, 5), range(22, 25), range(32, 35)):
+            resume.experience = i
+            with self.subTest(i=i):
+                self.assertEqual(resume.experience_str, '{} года'.format(i))
