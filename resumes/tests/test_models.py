4b27bd08c8a9be264a98728252e0198f749563c6
Vassili Baranov <vassili.baranov@gmail.com>
2019-01-24 14:44:17 +0200

    refactor Resume model tests

--- resumes/tests/test_models.py
+++ resumes/tests/test_models.py
@@ -9,43 +9,42 @@ User = get_user_model()
 
 
 class ResumeTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        User.objects.create_user('foo@bar.com', first_name='Иван', last_name='Иванов')
+    def setUp(self):
+        self.user = User.objects.create_user('foo@bar.com', first_name='Иван', last_name='Иванов')
+        self.resume = Resume.objects.create(user=self.user, position='Сварщик')
+        
 
     def test_str_resume(self):
-        user = User.objects.first()
-        resume = Resume.objects.create(user=user, position='Сварщик')
         expected = 'Резюме: сварщик Иван Иванов'
-        result = str(resume)
+        result = str(self.resume)
         self.assertEqual(expected, result)
 
     def test_salary_property(self):
-        user = User.objects.first()
-        resume = Resume.objects.create(user=user, position='Сварщик')
-        self.assertEqual(resume.salary, '')
-        resume.salary_min = 50000
-        self.assertEqual(resume.salary, 'от 50000')
-        resume.salary_max = 100000
-        self.assertEqual(resume.salary, '50000-100000')
-        resume.salary_min = 0
-        self.assertEqual(resume.salary, 'до 100000')
+        self.assertEqual(self.resume.salary, '')
+        self.resume.salary_min = 50000
+        self.assertEqual(self.resume.salary, 'от 50000')
+        self.resume.salary_max = 100000
+        self.assertEqual(self.resume.salary, '50000-100000')
+        self.resume.salary_min = 0
+        self.assertEqual(self.resume.salary, 'до 100000')
 
     def test_experience_str(self):
-        user = User.objects.first()
-        resume = Resume.objects.create(user=user, position='Сварщик')
-        self.assertEqual(resume.experience_str, '0 лет')
+        self.assertEqual(self.resume.experience_str, '0 лет')
         for i, exp in ((1, '1 год'), (21, '21 год'), (31, '31 год')):
-            resume.experience = i
+            self.resume.experience = i
             with self.subTest(i=i):
-                self.assertEqual(resume.experience_str, exp)
+                self.assertEqual(self.resume.experience_str, exp)
         
         for i in itertools.chain(range(5, 21), range(25, 31), range(35, 41)):
-            resume.experience = i
+            self.resume.experience = i
             with self.subTest(i=i):
-                self.assertEqual(resume.experience_str, '{} лет'.format(i))
+                self.assertEqual(self.resume.experience_str, '{} лет'.format(i))
 
         for i in itertools.chain(range(2, 5), range(22, 25), range(32, 35)):
-            resume.experience = i
+            self.resume.experience = i
             with self.subTest(i=i):
-                self.assertEqual(resume.experience_str, '{} года'.format(i))
+                self.assertEqual(self.resume.experience_str, '{} года'.format(i))
+
+    def test_get_absolute_url(self):
+        result = self.resume.get_absolute_url()
+        self.assertEqual(result, '/resumes/{}'.format(self.resume.id))
