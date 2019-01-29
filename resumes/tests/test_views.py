7c58dd173814532be422e770edbbf622ac7cf1dc
Vassili Baranov <vassili.baranov@gmail.com>
2019-01-29 17:43:44 +0200

    add ability to delete resume

--- resumes/tests/test_views.py
+++ resumes/tests/test_views.py
@@ -30,6 +30,43 @@ class ResumeDetailTestCase(TestCase):
         self.assertEqual(resp.context.get('resume'), resume)
 
 
+class ResumeDeleteTestCase(TestCase):
+    def test_with_unauthorized(self):
+        resp = self.client.get(reverse('resumes:resume_delete'))
+        self.assertRedirects(
+            resp,
+            '{}?next={}'.format(reverse('users:login'), reverse('resumes:resume_delete'))
+        )
+
+    def test_get_confirmation_view(self):
+        user = mixer.blend(User)
+        resume = mixer.blend(Resume, user=user)
+        self.client.force_login(user)
+
+        resp = self.client.get(reverse('resumes:resume_delete'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'resumes/resume_confirm_delete.html')
+
+    def test_post(self):
+        user = mixer.blend(User)
+        resume = mixer.blend(Resume, user=user)
+        self.assertEqual(Resume.objects.count(), 1)
+
+        self.client.force_login(user)
+
+        resp = self.client.post(reverse('resumes:resume_delete'))
+        self.assertRedirects(resp, reverse('mainapp:settings'))
+        self.assertEqual(Resume.objects.count(), 0)
+        user.refresh_from_db()
+        self.assertFalse(user.is_seeker)
+
+    def test_delete_nonexistent_resume(self):
+        user = User.objects.create_user('foo@bar.com', 'password')
+        self.client.force_login(user)
+        resp = self.client.post(reverse('resumes:resume_delete'))
+        self.assertEqual(resp.status_code, 404)
+
+
 class ResumeCreateTestCase(TestCase):
     url_name = 'resumes:resume_create'
 
