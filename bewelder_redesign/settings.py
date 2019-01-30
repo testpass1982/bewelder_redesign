b6319f385a0d7a2d6fd28d64c3e16ddd3e3dbedb
Vassili Baranov <vassili.baranov@gmail.com>
2019-01-30 19:02:26 +0200

    go to postgres

--- bewelder_redesign/settings.py
+++ bewelder_redesign/settings.py
@@ -82,9 +82,17 @@ WSGI_APPLICATION = 'bewelder_redesign.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bewgb_db',
+        'USER': 'bewgb_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
---
+++ postgres_db_create.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo -u postgres psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
+    CREATE DATABASE bewgb_db;
+    CREATE USER bewgb_user WITH PASSWORD 'password';
+    ALTER ROLE bewgb_user SET client_encoding TO 'utf8';
+    ALTER ROLE bewgb_user SET default_transaction_isolation TO 'read committed';
+    ALTER ROLE bewgb_user SET timezone TO 'UTC';
+    GRANT ALL PRIVILEGES ON DATABASE bewgb_db TO bewgb_user;
+    ALTER USER bewgb_user CREATEDB;
+EOSQL
