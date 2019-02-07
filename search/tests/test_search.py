cd5f10be3de48c5bad00741f6492e3c93f3d2cbb
Vassili Baranov <vassili.baranov@gmail.com>
2019-02-07 14:04:30 +0200

    refactor ResumeSearchTestCase

--- search/tests/test_search.py
+++ search/tests/test_search.py
@@ -23,6 +23,8 @@ TEST_INDEX = {
 
 @override_settings(HAYSTACK_CONNECTIONS=TEST_INDEX)
 class ResumeSearchTestCase(TestCase):
+    search_url = reverse('haystack_search')
+
     def setUp(self):
         mixer.cycle(5).blend(Resume, position='сварщик')
         mixer.cycle(3).blend(Resume, position='инженер')
@@ -32,30 +34,30 @@ class ResumeSearchTestCase(TestCase):
         super().setUp()
 
     def test_search_view(self):
-        resp = self.client.get(reverse('haystack_search'))
+        resp = self.client.get(self.search_url)
         self.assertEqual(resp.status_code, 200)
 
     def test_search_query(self):
-        resp = self.client.get(reverse('haystack_search'), {'q': 'сварщик'})
+        resp = self.client.get(self.search_url, {'q': 'сварщик'})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(len(resp.context['page'].object_list), 5)
         self.assertEqual(resp.context['page'].object_list[0].content_type(), 'resumes.resume')
 
-        resp = self.client.get(reverse('haystack_search'), {'q': 'инженер'})
+        resp = self.client.get(self.search_url, {'q': 'инженер'})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(len(resp.context['page'].object_list), 3)
         self.assertEqual(resp.context['page'].object_list[0].content_type(), 'resumes.resume')
 
-        resp = self.client.get(reverse('haystack_search'), {'q': 'директор'})
+        resp = self.client.get(self.search_url, {'q': 'директор'})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(len(resp.context['page'].object_list), 0)
 
     def test_realtime_update_search_index(self):
-        resp = self.client.get(reverse('haystack_search'), {'q': 'some position'})
+        resp = self.client.get(self.search_url, {'q': 'some position'})
         old_obj_count = len(resp.context['page'].object_list)
 
         mixer.blend(Resume, position='some position')
 
-        resp = self.client.get(reverse('haystack_search'), {'q': 'some position'})
+        resp = self.client.get(self.search_url, {'q': 'some position'})
         new_obj_count = len(resp.context['page'].object_list)
         self.assertEqual(new_obj_count, old_obj_count + 1)
