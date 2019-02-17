bc156ad8efaecf0e4983abea7ce67f1519ea6ab9
Vassili Baranov <vassili.baranov@gmail.com>
2019-02-17 19:44:23 +0200

    create a draft frontend application for the dialogs app

--- dialogs_front/components/App.js
+++ dialogs_front/components/App.js
@@ -1,18 +1,45 @@
 import React from "react";
 import Header from "./Header";
 import DialogList from "./DialogList";
+import { DIALOG_CREATE, DIALOG_LIST, DIALOG_VIEW } from "../constants/status";
+import DialogCreate from "./DialogCreate";
+import DialogView from "./DialogView";
+import { connect } from "react-redux";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    dialogs: []
+  };
 
   render() {
+    console.log("render App");
+    const status = this.props.status;
+    console.log("status:", status);
+    let view;
+    switch (status) {
+      case DIALOG_CREATE:
+        view = <DialogCreate />;
+        break;
+      case DIALOG_LIST:
+        view = <DialogList />;
+        break;
+      case DIALOG_VIEW:
+        view = <DialogView />;
+        break;
+      default:
+        view = <DialogList />;
+    }
     return (
-      <div className="container">
+      <div className="m-5">
         <Header />
-        <DialogList />
+        {view}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  status: state.status
+});
+
+export default connect(mapStateToProps)(App);
