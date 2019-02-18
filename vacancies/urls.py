e076f9784797170a649ae84f4581589148d17e66
Anatoliy Popov <popov.anatoly@gmail.com>
2019-02-18 17:06:32 +0300

    added view to vacancy details

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -5,6 +5,7 @@ import vacancies.views as vacancies
 app_name = 'vacancies'
 urlpatterns = [
     path('list/', vacancies.vacancies_list, name='list'),
+    path('details/<slug:pk>', vacancies.vacancy_details, name='vacancy_details'),
     path('new/', vacancies.add_new_vacancy, name='vacancy_create'),
     path('update/<slug:pk>', vacancies.vacancy_update, name='vacancy_update'),
     path('delete/<slug:pk>', vacancies.vacancy_delete, name='vacancy_delete'),
