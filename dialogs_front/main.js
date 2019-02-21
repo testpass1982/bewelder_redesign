f6f952e7dc1df06e3bc4596ccc75357d4d1033ac
Vassili Baranov <vassili.baranov@gmail.com>
2019-02-21 15:15:55 +0200

    add initial state for store

--- dialogs_front/main.js
+++ dialogs_front/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./containers/App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
@@ -12,7 +12,10 @@ api.init();
 $("#dialogs-modal").on("show.bs.modal", function() {
   console.log("start dialogs main");
   if (el) {
-    const store = createStore(reducer);
+    const initStore = {
+      dialogs: []
+    };
+    const store = createStore(reducer, initStore);
     ReactDOM.render(
       <Provider store={store}>
         <App />
