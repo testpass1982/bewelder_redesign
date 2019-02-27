faf1c958527f9b775a202756d0863d2ed34eabc1
Vassili Baranov <vassili.baranov@gmail.com>
2019-02-27 13:55:44 +0200

    add auto-scrolling to MessageList

--- dialogs_front/components/MessageList.js
+++ dialogs_front/components/MessageList.js
@@ -1,19 +1,38 @@
 import React from "react";
 import MessageListItem from "./MessageListItem";
 
-const MessageList = props => {
-  if (props.messages && props.messages.length) {
-    const messages = props.messages.map(message => (
-      <MessageListItem message={message} key={message.id} />
-    ));
-    return (
-      <div className="mb-3" style={{ height: "42vh", overflow: "auto" }}>
-        {messages}
-      </div>
-    );
-  } else {
-    return <div>Нет сообщений</div>;
+class MessageList extends React.Component {
+  messageList = React.createRef();
+
+  componentDidUpdate() {
+    this.scrollToBottom();
   }
-};
+
+  scrollToBottom = () => {
+    const messageList = this.messageList.current;
+    const { scrollHeight, clientHeight } = messageList;
+    const maxScrollTop = scrollHeight - clientHeight;
+    messageList.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+  };
+
+  render() {
+    if (this.props.messages && this.props.messages.length) {
+      const messages = this.props.messages.map(message => (
+        <MessageListItem message={message} key={message.id} />
+      ));
+      return (
+        <div
+          className="mb-3"
+          style={{ height: "42vh", overflow: "auto" }}
+          ref={this.messageList}
+        >
+          {messages}
+        </div>
+      );
+    } else {
+      return <div>Нет сообщений</div>;
+    }
+  }
+}
 
 export default MessageList;
