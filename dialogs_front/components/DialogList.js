4dd461c8436839b053263c995b60965ac1020215
Vassili Baranov <vassili.baranov@gmail.com>
2019-03-01 16:05:46 +0200

    add simple dialog filter (frontend)

--- dialogs_front/components/DialogList.js
+++ dialogs_front/components/DialogList.js
@@ -5,6 +5,10 @@ import TrashButton from "./TrashButton";
 import AnimateThis from "./AnimateThis";
 
 class DialogList extends React.Component {
+  state = {
+    filterValue: ""
+  };
+
   componentDidMount() {
     api.dialogs.get().then(dialogSet => {
       if (JSON.stringify(dialogSet) !== JSON.stringify(this.props.dialogSet)) {
@@ -17,24 +21,48 @@ class DialogList extends React.Component {
     api.dialogs.delete(id).then(() => this.props.deleteDialog(id));
   };
 
+  handleFilterChange = event => {
+    this.setState({ filterValue: event.target.value });
+  };
+
   render() {
-    const dialogs = this.props.dialogSet.map(dialog => (
-      <a
-        href="#"
-        className="list-group-item list-group-item-action "
-        key={dialog.id}
-        onClick={() => {
-          this.props.toDialogView(dialog.id);
-        }}
-      >
-        <div className="d-flex justify-content-between">
-          <DialogData dialog={dialog} />
-          <TrashButton onDelete={() => this.handleDelete(dialog.id)} />
-        </div>
-      </a>
-    ));
+    const dialogs = this.props.dialogSet
+      .filter(dialog => {
+        const creator = dialog.members.find(e => e.is_creator);
+        const s = `${creator.name} ${dialog.theme}`.toLowerCase();
+        return s.indexOf(this.state.filterValue.toLowerCase()) > -1;
+      })
+      .map(dialog => (
+        <a
+          href="#"
+          className="list-group-item list-group-item-action "
+          key={dialog.id}
+          onClick={() => {
+            this.props.toDialogView(dialog.id);
+          }}
+        >
+          <div className="d-flex justify-content-between">
+            <DialogData dialog={dialog} />
+            <TrashButton onDelete={() => this.handleDelete(dialog.id)} />
+          </div>
+        </a>
+      ));
+
     return (
       <div className="my-3">
+        <div className="form-group has-feedback">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Искать переписку"
+            value={this.state.filterValue}
+            onChange={this.handleFilterChange}
+          />
+          <i
+            className="fa fa-search text-success"
+            style={{ float: "right", margin: "-28px 10px" }}
+          />
+        </div>
         {dialogs.length ? (
           <div
             className="list-group"
