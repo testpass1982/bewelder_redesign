2fa25e029b5e367b78bcb25a776cacf23f2b923c
Anatoliy Popov <popov.anatoly@gmail.com>
2019-03-04 15:50:17 +0300

    working on vue loader

--- utils/postgres_db_drop.bash
+++ utils/postgres_db_drop.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 sudo -u postgres psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     DROP DATABASE IF EXISTS bewgb_db;
+    DROP DATABASE IF EXISTS test_bewgb_db;
     DROP USER IF EXISTS bewgb_user;
+    DROP USER IF EXISTS test_bewgb_db;
 EOSQL
